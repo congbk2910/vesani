5bf7351aa405c0b8554be159feb12e9af0563330
congbk2910 <congbk2910@gmail.com>
2022-05-20 14:30:19 +0700

    update sale rep column admin

---
+++ app/code/FME/ShareCart/Ui/Component/Listing/Column/SaleRep.php
@@ -0,0 +1,71 @@
+<?php
+namespace FME\ShareCart\Ui\Component\Listing\Column;
+
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+
+class SaleRep extends \Magento\Ui\Component\Listing\Columns\Column
+{
+    /**
+     * Column name
+     */
+    const NAME = 'column.sale_rep';
+
+    /**
+     * @var \Magento\Framework\Locale\CurrencyInterface
+     */
+    protected $localeCurrency;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param \Magento\Framework\Locale\CurrencyInterface $localeCurrency
+     * @param \Magento\Store\Model\StoreManagerInterface $storeManager
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        \Magento\Framework\Locale\CurrencyInterface $localeCurrency,
+        \Magento\Store\Model\StoreManagerInterface $storeManager,
+        \Magento\Customer\Model\CustomerFactory $customerFactory,
+        \Magento\Framework\UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+        $this->localeCurrency = $localeCurrency;
+        $this->storeManager = $storeManager;
+        $this->customerFactory = $customerFactory;
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        $saleRepId = '';
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                $saleRepId = $item['sale_rep_id'];
+                $saleRep = $this->customerFactory->create()->load($saleRepId);
+
+                $item[$this->getData('name')]['edit'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'customer/index/edit',
+                        ['id' => $saleRepId]
+                    ),
+                    'label' => __($saleRep->getName()),
+                    'hidden' => false,
+                ];
+            }
+        }
+
+        return $dataSource;
+    }
+}
