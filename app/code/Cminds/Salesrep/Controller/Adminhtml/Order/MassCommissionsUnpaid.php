577c9459e071140ac5f9382f7ef04767b98f8834
congbk2910 <congbk2910@gmail.com>
2022-05-20 15:52:53 +0700

    remove Cminds Salerep

--- app/code/Cminds/Salesrep/Controller/Adminhtml/Order/MassCommissionsUnpaid.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Cminds\Salesrep\Controller\Adminhtml\Order;
-
-class MassCommissionsUnpaid extends AbstractMassCommissions
-{
-    protected $value = 'Unpaid';
-}
--- app/code/Cminds/Salesrep/registration.php
+++
@@ -1,4 +0,0 @@
-<?php
-use \Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Cminds_Salesrep', __DIR__);
