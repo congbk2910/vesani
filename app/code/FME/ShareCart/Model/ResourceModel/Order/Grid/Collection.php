cea0747cd44c2ce1f7a4fe89cb53ad00c50ab67f
congbk2910 <congbk2910@gmail.com>
2022-05-20 14:41:57 +0700

    change sale_rep column into sale_rep_id

--- app/code/FME/ShareCart/Model/ResourceModel/Order/Grid/Collection.php
+++ app/code/FME/ShareCart/Model/ResourceModel/Order/Grid/Collection.php
@@ -9,7 +9,7 @@ class Collection extends OriginalCollection
     protected function _renderFiltersBefore()
     {
         $joinTable = $this->getTable('sales_order');
-        $this->getSelect()->joinLeft($joinTable, 'main_table.entity_id = sales_order.entity_id', ['sale_rep']);
+        $this->getSelect()->joinLeft($joinTable, 'main_table.entity_id = sales_order.entity_id', ['sale_rep_id']);
         parent::_renderFiltersBefore();
     }
 }
