dabe4d1a6f51d32e3a7677f0e3266127648f9bba
congbk2910 <congbk2910@gmail.com>
2022-05-24 18:24:47 +0700

    update salerep report

---
+++ app/code/FME/ShareCart/Controller/Adminhtml/Report/Index.php
@@ -0,0 +1,41 @@
+<?php
+namespace FME\ShareCart\Controller\Adminhtml\Report;
+ 
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends \Magento\Backend\App\Action
+{
+    const ADMIN_RESOURCE = 'FME_ShareCart::salerep_reports';
+    /**
+    * @var \Magento\Framework\View\Result\PageFactory
+    */
+    protected $resultPageFactory;
+
+    /**
+     * Constructor
+     *
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $resultPageFactory
+    ) {
+         parent::__construct($context);
+         $this->resultPageFactory = $resultPageFactory;
+    }
+
+    /**
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->resultPageFactory->create();
+        $resultPage->setActiveMenu('FME_ShareCart::salerep_reports');
+        $resultPage->getConfig()->getTitle()->prepend(__('Sale Representative Reports'));
+        return $resultPage;
+    }
+  }
